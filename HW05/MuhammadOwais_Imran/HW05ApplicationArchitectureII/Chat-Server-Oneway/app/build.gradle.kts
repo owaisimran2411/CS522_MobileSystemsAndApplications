9045b2eaf93cf8ee631ae6eb244d958497595b06
Owais <owaisimran678@gmail.com>
2024-02-23 17:42:56 -0500

    Add files for HW05

---
+++ HW05/MuhammadOwais_Imran/HW05ApplicationArchitectureII/Chat-Server-Oneway/app/build.gradle.kts
@@ -0,0 +1,103 @@
+plugins {
+    id("com.android.application")
+}
+
+class RoomSchemaArgProvider(
+        @get:InputDirectory
+        @get:PathSensitive(PathSensitivity.RELATIVE)
+        val schemaDir: File
+) : CommandLineArgumentProvider {
+    override fun asArguments(): Iterable<String> {
+        // Note: If you're using KSP, change the line below to return
+        // listOf("room.schemaLocation=${schemaDir.path}").
+        return listOf("-Aroom.schemaLocation=${schemaDir.path}")
+    }
+}
+
+android {
+    namespace = "edu.stevens.cs522.chatserver"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "edu.stevens.cs522.chatserver"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                compilerArgumentProviders(
+                        RoomSchemaArgProvider(File(projectDir, "schemas"))
+                )
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+// Hack to get around overlapping dependencies added by Kotlin libraries
+configurations.implementation {
+    exclude ("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
+}
+
+dependencies {
+
+    // Dependencies for RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    // For control over item selection of both touch and mouse driven selection
+    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+
+    // Dependencies for fragments (need FragmentActivity for LifeCycleOwner)
+    implementation("androidx.fragment:fragment:1.6.2")
+
+    // Dependencies for LifeCycle, ViewModel, LiveData
+    val lifecycle_version = "2.7.0"
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata:$lifecycle_version")
+    // Lifecycles only (without ViewModel or LiveData)
+    implementation("androidx.lifecycle:lifecycle-runtime:$lifecycle_version")
+
+    // Saved state module for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
+
+    // Annotation processor
+    // annotationProcessor "androidx.lifecycle:lifecycle-compiler:$lifecycle_version"
+    // alternately - if using Java8, use the following instead of lifecycle-compiler
+    implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycle_version")
+
+    // optional - Test helpers for LiveData
+    // testImplementation("androidx.arch.core:core-testing:$arch_version")
+
+
+    // Dependencies for the Room ORM
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    // testImplementation "androidx.room:room-testing:$room_version"
+
+    implementation(files("libs/cs522-library.aar"))
+    implementation("com.google.guava:guava:33.0.0-android")
+    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
